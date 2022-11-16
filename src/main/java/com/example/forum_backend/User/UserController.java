1bc95f488d03b721dc8e76f605fcee64a0127aad
Tapio Koskinen <tapio.koskinen@solita.fi>
2022-11-16 13:18:34 +0200

    hello world from back to front

--- src/main/java/com/example/forum_backend/User/UserController.java
+++ src/main/java/com/example/forum_backend/User/UserController.java
@@ -1,16 +1,22 @@
 package com.example.forum_backend.User;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api")
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class UserController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    public List<User> hello() {
+        return List.of(
+                new User(1L,"hello","@world","hlowrld")
+        );
     }
 
 }
---
+++ src/main/java/com/example/forum_backend/User/UserService.java
@@ -0,0 +1,8 @@
+package com.example.forum_backend.User;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+}
