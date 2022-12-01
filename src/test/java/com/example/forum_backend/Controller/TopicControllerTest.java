a8159a9453ea4d1a00136f933ac4a12eb6f31922
Tapio Koskinen <tapio.koskinen@solita.fi>
2022-12-01 11:17:21 +0200

    TopicController tests

---
+++ src/test/java/com/example/forum_backend/Controller/TopicControllerTest.java
@@ -0,0 +1,160 @@
+package com.example.forum_backend.Controller;
+
+import com.example.forum_backend.Comment.Comment;
+import com.example.forum_backend.Comment.CommentDto;
+import com.example.forum_backend.Topic.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(controllers = TopicController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class TopicControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TopicService topicService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Topic topic;
+    private TopicDto topicDto;
+    private Comment comment;
+    private CommentDto commentDto;
+
+    @BeforeEach
+    public void init() {
+        topic = new Topic();
+        topic.setTitle("Unit test");
+        topic.setOwner(1L);
+        topic.setCreatedAt(new Date());
+        topic.setUpdatedAt(new Date());
+
+        topicDto = new TopicDto();
+        topicDto.setTitle("Unit test");
+        topicDto.setOwner_id(1L);
+        topicDto.setCreatedAt(new Date());
+        topicDto.setUpdatedAt(new Date());
+
+        comment = new Comment();
+        comment.setComment("Unit test");
+        comment.setOwner(1L);
+        comment.setCreatedAt(new Date());
+        comment.setUpdatedAt(new Date());
+
+        commentDto = new CommentDto();
+        commentDto.setOwner(1L);
+        commentDto.setComment("Unit test");
+        commentDto.setCreatedAt(new Date());
+        commentDto.setUpdatedAt(new Date());
+
+    }
+
+    @Test
+    public void TopicController_CreateTopic_ReturnCreated() throws Exception {
+
+        given(topicService.addTopic(ArgumentMatchers.any())).willAnswer((invocation -> invocation.getArgument(0)));
+
+        ResultActions response = mockMvc.perform(post("/api/topics/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(topicDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(topicDto.getTitle())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.owner_id", CoreMatchers.is(topicDto.getOwner_id()), Long.class))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void TopicController_GetAllTopics_ReturnResponseDto() throws Exception {
+
+        TopicResponse responseDto = new TopicResponse();
+        responseDto.setPageNo(1);
+        responseDto.setPageSize(10);
+        responseDto.setLast(true);
+        responseDto.setContent(Arrays.asList(topicDto));
+        when(topicService.getAllTopics(1,10)).thenReturn(responseDto);
+
+        ResultActions response = mockMvc.perform(get("/api/topics")
+                .param("pageNo", "1")
+                .param("pageSize", "10"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.size()", CoreMatchers.is(responseDto.getContent().size())));
+
+    }
+
+    @Test
+    public void TopicController_TopicDetail_ReturnResponseDto() throws Exception {
+
+        long topicId = 1L;
+        when(topicService.getTopicById(topicId)).thenReturn(topicDto);
+
+        ResultActions response = mockMvc.perform(get("/api/topics/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(topicDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(topicDto.getTitle())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.owner_id", CoreMatchers.is(topicDto.getOwner_id()), Long.class))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void TopicController_UpdateTopic_ReturnTopicDto() throws Exception {
+        topicDto.setId(1L);
+        TopicResponse responseDto = new TopicResponse();
+        responseDto.setPageNo(1);
+        responseDto.setPageSize(10);
+        responseDto.setLast(true);
+        responseDto.setContent(Arrays.asList(topicDto));
+        when(topicService.updateTopic(topicDto,1L)).thenReturn(topicDto);
+
+        ResultActions response = mockMvc.perform(put("/api/topics/{id}/update",1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(topicDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());;
+
+    }
+
+    @Test
+    public void TopicController_DeleteTopic_ReturnString() throws Exception {
+        Long id = 1L;
+        doNothing().when(topicService).deleteTopic(id);
+
+        ResultActions response = mockMvc.perform(delete("/api/topics/1/delete")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+}
