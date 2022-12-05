20539de98cdccb7a2a7bd7401ace4c51d314f574
Tapio Koskinen <tapio.koskinen@solita.fi>
2022-12-05 08:35:04 +0200

    Allow global cors from localhost:3000 & longer jwt expiration

--- src/main/java/com/example/forum_backend/security/SecurityConfig.java
+++ src/main/java/com/example/forum_backend/security/SecurityConfig.java
@@ -12,10 +12,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     private JwtAuthEntryPoint authEntryPoint;
     private CustomUserDetailsService userDetailsService;
@@ -25,10 +32,13 @@ public class SecurityConfig {
         this.authEntryPoint = authEntryPoint;
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(authEntryPoint)
                 .and()
@@ -59,4 +69,16 @@ public class SecurityConfig {
     public JWTAuthenticationFilter jwtAuthenticationFilter() {
         return new JWTAuthenticationFilter();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
--- src/main/java/com/example/forum_backend/security/SecurityConstants.java
+++ src/main/java/com/example/forum_backend/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.example.forum_backend.security;
 
 public class SecurityConstants {
 
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 600000;
     public static final String JWT_SECRET = "secret";
 
 }
